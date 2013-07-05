4b44b5ed52d0fdb537bf6f41d38c8c8458ae61a6
Frederic de Laminne <fdl-coursera@event-pics.be>
2013-07-05 20:21:51 +0000

    web.js reads from html page

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var html = fs.readFileSync("index.html"),'utf8');
+  var html = fs.readFileSync('index.html','utf8');
   response.send(html);
 });
 
